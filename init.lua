c832ef606fba1cea0ce8ccdc7527a6e05fe1f453
rxi <rxi@users.noreply.github.com>
2020-04-20 19:15:05 +0100

    Changed logging to use core log functions instead of print()

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local _, mpath = ...
 local M = {dir = mpath:gsub("init.lua$", "")}
 
+local core    = require "core"
 local style   = require "core.style"
 local common  = require "core.common"
 local command = require "core.command"
@@ -8,7 +9,6 @@ local command = require "core.command"
 
 local _sf = string.format
 local _mf = math.floor
-local _printf = function(...) print(_sf(...)) end
 
 local split = function(s)
 	local args = {}
@@ -53,7 +53,7 @@ local to_hsl = function(r, g, b)
 	r, g, b = r / 0xFF, g / 0xFF, b / 0xFF
 	local max, min = math.max(r, g, b), math.min(r, g, b)
 	if max == min then return 0, 0, min end
-	
+
 	local l, d = max + min, max - min
 	local s, h = d / (l > 1 and (2 - l) or l)
 	l = l / 2
@@ -74,12 +74,12 @@ local FF = function(a) return _mf(a * 0xFF) end
 local to_rgb = function(h, s, l)
 	--h, s, l = h % 6, clamp(s, 0, 1), clamp(l, 0, 1)
 	if s == 0 then l = FF(l) return l,l,l end
-	
+
 	local c, m
 	if l > 0.5 then c = (2 - 2 * l) * s
 	else c = (2 * l) * s end
 	m = l - c / 2
-	
+
 	local r, g, b
 	if     h < 1 then b, r, g = 0, c, c * h
 	elseif h < 2 then b, g, r = 0, c, c * (2 - h)
@@ -172,8 +172,8 @@ local parse_scheme = function(path, ext)
 		end
 	end
 	for id, k in pairs(map) do
-		if vars[k] == nil then 
-		_printf('missing value %s in %s', id, path) return end
+		if vars[k] == nil then
+		core.error('missing value %s in %s', id, path) return end
 	end
 	return vars
 end
@@ -207,10 +207,10 @@ end
 
 local locate_scheme = function(pdir, name)
 	if isfile(pdir .. name) then return name end
-	
+
 	local search = {'%s.yaml', 'base16/%s', 'base16/%s.yaml',
 		'%s.json', 'daylerees/%s', 'daylerees/%s.json'}
-	
+
 	local s
 	for i, fmt in ipairs(search) do
 		s = _sf(fmt, name)
@@ -220,31 +220,31 @@ end
 
 local apply_scheme = function(name)
 	local schemedir = M.dir .. 'schemes/'
-	
+
 	local sadj = M.saturation or 1.0
 	local ladj = M.lightness  or 1.0
 	--local wadj = M.whitespace or 0.5
-	
+
 	local scheme_path = locate_scheme(schemedir, name)
 	if not scheme_path then
-		_printf('File "%s" cannot be found', name)
+		core.error('File "%s" cannot be found', name)
 		return
 	end
-	
+
 	name = scheme_path
 	scheme_path = schemedir .. scheme_path
-	
+
 	local filetype = scheme_path:sub(-5)
 	local vars = parse_scheme(scheme_path, filetype)
 	if vars == nil then
-		_printf('Filetype "%s" is not supported', filetype)
+		core.error('Filetype "%s" is not supported', filetype)
 		return
 	end
 
 	adjust_colors(vars, sadj, ladj)
 	--local ws = vars.whitespace
 	--vars.whitespace = {ws[1], ws[2], adjmix(wadj, ws[3], vars.back[3], vars.fore[3])}
-	
+
 	style.background     = toRGB(vars["back"])
 	style.background2    = toRGB(vars["back"])
 	style.background3    = { common.color "#ff0000" } --????
@@ -270,14 +270,14 @@ local apply_scheme = function(name)
 	style.syntax["string"]   = toRGB(vars["string"])
 	style.syntax["operator"] = toRGB(vars["operator"])
 	style.syntax["function"] = toRGB(vars["function"])
-	
+
 	M.current = name
 end
 
 local change_theme = function()
 	local name = M.name:lower():gsub(' ','-'):gsub('[,]','')
 	apply_scheme(name)
-	_printf('Using "%s"', M.current)
+	core.log_quiet('Using "%s"', M.current)
 end
 
 local cycle_theme = function(step)
@@ -297,7 +297,7 @@ local cycle_theme = function(step)
 	list_cur = 1 + ((list_cur + step - 1) % list_len)
 	name = list[list_cur]
 	apply_scheme(name)
-	_printf('Using "%s" %i/%i', M.current, list_cur, list_len)
+	core.log('Using "%s" %i/%i', M.current, list_cur, list_len)
 end
 
 M.apply = change_theme
